b07de1be25090524a3f4ccb16225754e3bbbb1bf
The Coding Montana <63234437+DevHumbleChris@users.noreply.github.com>
2023-07-21 14:13:37 +0000

    successfully added fonts

--- components/hero/Header.vue
+++ components/hero/Header.vue
@@ -1,5 +1,27 @@
+<script setup>
+import Logo from "../global/Logo.vue";
+</script>
+
 <template>
-    <div>
-        <h1>Hello</h1>
+  <div class="p-4 flex items-center w-full justify-between font-poppins">
+    <Logo />
+    <div class="flex items-center space-x-20">
+      <ul class="flex items-center space-x-12">
+        <li><a href="">Destinations</a></li>
+        <li><a href="">Hotels</a></li>
+        <li><a href="">Flights</a></li>
+        <li><a href="">Bookings</a></li>
+      </ul>
+      <ul class="flex items-center space-x-8">
+        <li><a href="">Login</a></li>
+        <li><a href="">Signup</a></li>
+        <li>
+          <select name="country">
+            <option value="EN" selected>EN</option>
+            <option value="FRA">FRA</option>
+          </select>
+        </li>
+      </ul>
     </div>
+  </div>
 </template>
