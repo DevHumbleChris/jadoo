d22e5aad217fba327b024bfc0eebdf393c86c191
DevHumbleChris <dev.humblechris254@gmail.com>
2023-07-23 23:27:23 +0300

    added scroll on companies component

--- components/companies/index.vue
+++ components/companies/index.vue
@@ -3,12 +3,22 @@
 
 <template>
     <section class="p-6 sm:p-20 font-poppins">
-        <div class="flex items-center space-x-16 justify-center">
-            <nuxt-picture src="/images/axon.jpg" alt="axon" />
-            <nuxt-picture src="/images/jestar.jpg" alt="jestar" />
-            <nuxt-picture src="/images/expedia.jpg" alt="expedia" />
-            <nuxt-picture src="/images/qantas.jpg" alt="qantas" />
-            <nuxt-picture src="/images/alitalia.jpg" alt="alitalia" />
+        <div class="flex items-center space-x-16 justify-center shrink-0 overflow-x-scroll companies">
+            <nuxt-picture src="/images/axon.jpg" alt="axon" class="shrink-0" />
+            <nuxt-picture src="/images/jestar.jpg" alt="jestar" class="shrink-0"  />
+            <nuxt-picture src="/images/expedia.jpg" alt="expedia" class="shrink-0"  />
+            <nuxt-picture src="/images/qantas.jpg" alt="qantas" class="shrink-0"  />
+            <nuxt-picture src="/images/alitalia.jpg" alt="alitalia" class="shrink-0"  />
         </div>
     </section>
 </template>
+
+<style scoped>
+.companies::-webkit-scrollbar {
+    height: 10px;
+}
+.companies::-webkit-scrollbar-thumb {
+    background: #28508d;
+    border-radius: 10px;
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,7 +17,10 @@ export default defineNuxtConfig({
           'poppins': ['Poppins', 'sans-serif'],
           'volkhov': ['Volkhov', 'serif']
         }
-      }
+      },
+      plugins: [
+        require('tailwind-scrollbar-hide')
+      ]
     }
   },
   pinia: {
