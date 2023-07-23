ad182c1e9b194d98cb3594d0273d9cca81dd641d
DevHumbleChris <dev.humblechris254@gmail.com>
2023-07-23 10:31:44 +0300

    saving changes

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,32 +1,16 @@
 <script setup>
+const route = useRoute()
+
 useHead({
   meta: [
-    { property: "og:title", content: `Jadoo` },
-    {
-      name: "description",
-      content:
-        "Jadoo is a travel agency landing page built with Nuxt 3 and TailwindCSS.",
-    },
-    {
-      property: "og:description",
-      content:
-        "Jadoo is a travel agency landing page built with Nuxt 3 and TailwindCSS.",
-    },
-    { property: "og:url", content: "https://nuxt-gmail-clone.vercel.app" },
-    { name: "twitter:title", content: "Jadoo" },
-    {
-      name: "twitter:description",
-      content:
-        "Jadoo is a travel agency landing page built with Nuxt 3 and TailwindCSS.",
-    },
-    { name: "twitter:site", content: "https://nuxt-gmail-clone.vercel.app" },
+    { property: 'og:title', content: `Jadoo` },
+    { name: 'description', content: 'Jadoo is a travel agency landing page built with Nuxt 3 and TailwindCSS.' },
+    { property: 'og:description', content: 'Jadoo is a travel agency landing page built with Nuxt 3 and TailwindCSS.' },
+    { property: 'og:url', content: 'https://nuxt-gmail-clone.vercel.app' },
+    { name: 'twitter:title', content: 'Jadoo' },
+    { name: 'twitter:description', content: 'Jadoo is a travel agency landing page built with Nuxt 3 and TailwindCSS.' },
+    { name: 'twitter:site', content: 'https://nuxt-gmail-clone.vercel.app' }
   ],
-  title: `Jadoo`,
-});
+  title: `${route.meta.title}`,
+})
 </script>
-
-<template>
-  <main>
-    <slot />
-  </main>
-</template>
