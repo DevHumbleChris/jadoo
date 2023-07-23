8b2e25eae58beeb9e25c896e3f42911e47274c97
DevHumbleChris <dev.humblechris254@gmail.com>
2023-07-23 23:28:37 +0300

    added links of the website to SEO

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,10 +5,10 @@ useHead({
     { property: 'og:title', content: `Jadoo` },
     { name: 'description', content: 'Jadoo is a travel agency landing page built with Nuxt 3 and TailwindCSS.' },
     { property: 'og:description', content: 'Jadoo is a travel agency landing page built with Nuxt 3 and TailwindCSS.' },
-    { property: 'og:url', content: 'https://nuxt-gmail-clone.vercel.app' },
+    { property: 'og:url', content: 'https://jadoo-ke.vercel.app/' },
     { name: 'twitter:title', content: 'Jadoo' },
     { name: 'twitter:description', content: 'Jadoo is a travel agency landing page built with Nuxt 3 and TailwindCSS.' },
-    { name: 'twitter:site', content: 'https://nuxt-gmail-clone.vercel.app' }
+    { name: 'twitter:site', content: 'https://jadoo-ke.vercel.app/' }
   ],
   title: `Jadoo`,
 })
